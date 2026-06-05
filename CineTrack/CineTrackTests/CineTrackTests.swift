5a7a7cb6f58950cfad6ddf955752713c6dbf75cf
GegiGhvachliani <gegighvachliani@gmail.com>
2026-06-05 22:31:32 +0400

    chore: initialize Xcode project with iOS 16 target and UIKit lifecycle

---
+++ CineTrack/CineTrackTests/CineTrackTests.swift
@@ -0,0 +1,19 @@
+//
+//  CineTrackTests.swift
+//  CineTrackTests
+//
+//  Created by Gegi Ghvachliani on 05/06/2026.
+//
+
+import Testing
+@testable import CineTrack
+
+struct CineTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
