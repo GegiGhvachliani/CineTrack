3d12650accccef93a81f1bec5f6155f154ec0083
GegiGhvachliani <gegighvachliani@gmail.com>
2026-06-08 00:32:08 +0400

    add dependencies for: - DesignSystem

--- CineTrack/CineTrack/Packages/DesignSystem/Package.swift
+++ CineTrack/CineTrack/Packages/DesignSystem/Package.swift
@@ -5,18 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "DesignSystem",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DesignSystem",
             targets: ["DesignSystem"]
         ),
     ],
+    dependencies: [
+        .package(path: "../SharedKit")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystem"
+            name: "DesignSystem",
+            dependencies: ["SharedKit"]
         ),
         .testTarget(
             name: "DesignSystemTests",
