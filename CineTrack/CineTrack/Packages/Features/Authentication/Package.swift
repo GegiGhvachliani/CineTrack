89bb14c972cccb7a6df62e053b58b74173d98339
Gegi Ghvachliani <gegighvachliani@gmail.com>
2026-06-29 17:32:33 +0400

    add Auth package with initial layers

---
+++ CineTrack/CineTrack/Packages/Features/Authentication/Package.swift
@@ -0,0 +1,84 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Authentication",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(name: "AuthenticationAssembly", targets: ["AuthenticationAssembly"]),
+        .library(name: "AuthenticationPresentationAPI", targets: ["AuthenticationPresentationAPI"]),
+        
+        
+    ],
+    
+    dependencies: [
+        .package(path: "../../SharedKit"),
+        .package(path: "../../DesignSystem"),
+    ],
+    targets: [
+
+        .target(
+            name: "AuthenticationDomain",
+            dependencies: [
+                .product(name: "SharedCore", package: "SharedKit")
+            ],
+            path: "Sources/AuthenticationDomain"
+        ),
+
+        .target(
+            name: "AuthenticationData",
+            dependencies: [
+                "AuthenticationDomain",
+                .product(name: "SharedNetworking", package: "SharedKit"),
+                .product(name: "SharedStorage", package: "SharedKit"),
+            ],
+            path: "Sources/AuthenticationData"
+        ),
+
+        .target(
+            name: "AuthenticationPresentation",
+            dependencies: [
+                "AuthenticationDomain",
+                "AuthenticationPresentationAPI",
+                .product(name: "SharedCore", package: "SharedKit"),
+                .product(
+                    name: "DesignSystemComponents",
+                    package: "DesignSystem"
+                ),
+            ],
+            path: "Sources/AuthenticationPresentation"
+        ),
+
+        .target(
+            name: "AuthenticationPresentationAPI",
+            dependencies: [
+                .product(name: "SharedCore", package: "SharedKit")
+            ],
+            path: "Sources/AuthenticationPresentationAPI"
+        ),
+
+        .target(
+            name: "AuthenticationAssembly",
+            dependencies: [
+                "AuthenticationDomain",
+                "AuthenticationData",
+                "AuthenticationPresentation",
+                "AuthenticationPresentationAPI",
+            ],
+            path: "Sources/AuthenticationAssembly"
+        ),
+
+        .testTarget(
+            name: "AuthenticationTests",
+            dependencies: [
+                "AuthenticationDomain",
+                "AuthenticationData",
+                "AuthenticationPresentation",
+            ],
+            path: "Tests"
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
